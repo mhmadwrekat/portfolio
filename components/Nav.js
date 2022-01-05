79497a110992779e212622cfcf60907c18539013
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-01-05 17:08:12 +0200

    test

---
+++ components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react' ;
+import Head from 'next/head';
+import Link from "next/link";
+
+const Nav = () => {
+    return (
+        <>
+        <Head>
+            <title>Mhmad Wrekat</title>
+            <link rel="icon" href="./icon.png" />
+        </Head>
+        </>
+    )
+}
+
+export default Nav
---
+++ pages/profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+const profile = () => {
+    return (
+        <>
+<Nav/>            
+        </>
+    )
+}
+
+export default profile
