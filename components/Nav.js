1215a6fe44c555dc942185453bd570d407717cb6
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-01-14 06:28:38 +0200

    Edit

--- components/Nav.js
+++ components/Nav.js
@@ -34,35 +34,35 @@ const Nav = () => {
                         </a>
                         <button class="mobile-menu-button" type="button" >
                             <span class="sr-only">Open main menu</span>
-                            <svg class="md:hidden flex w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                            <svg class="md:hidden flex w-6 h-6"  viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                         </button>
                     </div>
                     <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-4">
                         <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                             <Link href='/'>
                                 <li>
-                                    <a href="" class="cursor-grab hover:scale-110 block py-2 pr-4 pl-3 text-gray-200 hover:text-violet-600 rounded md:bg-transparent md:p-0" aria-current="page">Profile</a>
+                                    <p class="cursor-grab hover:scale-110 block py-2 pr-4 pl-3 text-gray-200 hover:text-violet-600 rounded md:bg-transparent md:p-0">Profile</p>
                                 </li>
                             </Link>
                             <Link href='/projects'>
                                 <li>
-                                    <a href="" class="cursor-grab hover:scale-110 block py-2 pr-4 pl-3 text-gray-200 hover:text-violet-600 md:border-0 md:p-0">Project</a>
+                                    <p class="cursor-grab hover:scale-110 block py-2 pr-4 pl-3 text-gray-200 hover:text-violet-600 md:border-0 md:p-0">Project</p>
                                 </li>
                             </Link>
                         </ul>
                     </div>
                 </div>
             </nav>
-            <div class='hidden mobile-menu md:hidden bg-black opacity-80 border-gray-200'>
-                <ul class="flex flex-col md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            <div class='hidden mobile-menu md:hidden w-full bg-black opacity-80 border-gray-200'>
+                <ul class="flex flex-col md:flex-row md:space-x-0 md:mt-0 md:text-sm md:font-medium text-center">
                     <Link href='/'>
-                        <li>
-                            <a href="" class="cursor-grab hover:scale-110 block py-2 pr-4 pl-3 text-gray-200 text-center hover:text-violet-600 rounded md:p-0 " aria-current="page">Profile</a>
+                        <li className='py-3'>
+                            <a class="cursor-grab hover:scale-110 py-2  text-gray-200 text-center hover:text-violet-600 rounded md:p-0">Profile</a>
                         </li>
                     </Link>
                     <Link href='/projects'>
-                        <li>
-                            <a href="" class="cursor-grab hover:scale-110 block py-2 pr-4 pl-3 text-center text-gray-200 hover:text-violet-600 md:p-0">Project</a>
+                        <li className='py-3'>
+                            <a href="" class="cursor-grab hover:scale-110 py-2  text-center text-gray-200 hover:text-violet-600 md:p-0">Project</a>
                         </li>
                     </Link>
                 </ul>
