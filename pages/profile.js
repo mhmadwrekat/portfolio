b4bb77bd75ceb86aabaf8fb5429a1bcf39cf894e
Mhmad Wrekat <86598070+mhmadwrekat@users.noreply.github.com>
2022-01-16 08:58:28 +0200

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -42,12 +42,12 @@ bg-[url('https://cutewallpaper.org/21/3000x3000-wallpaper/3000x3000-gradient-bac
                             <div className="bg-[url('https://github.com/ai-survivors/world_cup_22_frontend/blob/main/assest/wrekat.jpg?raw=true')] block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center">
                             </div>
                             <h1 className="pt-8 text-3xl font-bold lg:pt-0 ">
-                                <Flip>
+                                <Flip triggerOnce>
                                     <p>Mhmad Wrekat</p>
                                 </Flip>
                             </h1>
                             <div className="w-4/5 pt-3 mx-auto border-b-2 opacity-25 border-violet-900 lg:mx-0"></div>
-                            <Fade delay={1000}>
+                            <Fade triggerOnce delay={1000}>
                                 <p className="flex items-center justify-center pt-4 text-base font-bold lg:justify-start">
                                     <svg
                                         className="h-4 pr-4 fill-current text-violet-800" xmlns="http://www.w3.org/2000/svg"
@@ -57,7 +57,7 @@ bg-[url('https://cutewallpaper.org/21/3000x3000-wallpaper/3000x3000-gradient-bac
                                     </svg>
                                     Web Developer</p>
                             </Fade>
-                            <Fade delay={1500}>
+                            <Fade triggerOnce delay={1500}>
                                 <p
                                     className="flex items-center justify-center pt-2 pr-5 text-xs text-white-600 lg:text-sm lg:justify-start">
                                     <svg className="h-4 pr-4 fill-current text-violet-800" xmlns="http://www.w3.org/2000/svg"
@@ -66,15 +66,14 @@ bg-[url('https://cutewallpaper.org/21/3000x3000-wallpaper/3000x3000-gradient-bac
                                             d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z" />
                                     </svg>Jordan - Amman
                                 </p></Fade>
-                            <Fade delay={2000}>
+                            <Fade triggerOnce delay={2000}>
                                 <p className="pt-8 text-sm">
                                     Have a Beclore Degree in Software Engineering ,
                                     and Web Development - FrontEnd and BackEnd ,
-                                    I like Programming and Web Development .
-                                    The Excites Me The Most About Tech is it Always Evolve Quickly and New
-                                    Technologies are Always Emerging .</p>
+                                    and i am "Open To Work" .
+                                    </p>
                             </Fade>
-                            <Fade delay={2500}>
+                            <Fade triggerOnce delay={2500}>
                                 <div className="pt-12 pb-8">
                                     <a target='_balnk' href="https://wa.me/+962798596928">
                                         <span class=" h-0 w-0">
