cdb2d8b5aba6d97782c9e260c1645b2383b12a11
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-06-23 19:47:10 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ const index = (props) => {
       </section>
       <noscript>
         <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-NXZSBVW"
+          src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}`}
           height="0"
           width="0"
           className="hidden"
