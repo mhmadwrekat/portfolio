694dac757a9f7a24bbe1109f239341d4f429cfce
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-07-03 13:51:15 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -29,17 +29,82 @@ function MyApp({ Component, pageProps }) {
         }}
       />
       {/* L6uzByobzaeXDxTEp4EKyDXuK0LW8yTa */}
+      <script>
+        {
+          !(function () {
+            var analytics = (window.analytics = window.analytics || []);
+            if (!analytics.initialize)
+              if (analytics.invoked)
+                window.console &&
+                  console.error &&
+                  console.error("Segment snippet included twice.");
+              else {
+                analytics.invoked = !0;
+                analytics.methods = [
+                  "trackSubmit",
+                  "trackClick",
+                  "trackLink",
+                  "trackForm",
+                  "pageview",
+                  "identify",
+                  "reset",
+                  "group",
+                  "track",
+                  "ready",
+                  "alias",
+                  "debug",
+                  "page",
+                  "once",
+                  "off",
+                  "on",
+                  "addSourceMiddleware",
+                  "addIntegrationMiddleware",
+                  "setAnonymousId",
+                  "addDestinationMiddleware",
+                ];
+                analytics.factory = function (e) {
+                  return function () {
+                    var t = Array.prototype.slice.call(arguments);
+                    t.unshift(e);
+                    analytics.push(t);
+                    return analytics;
+                  };
+                };
+                for (var e = 0; e < analytics.methods.length; e++) {
+                  var key = analytics.methods[e];
+                  analytics[key] = analytics.factory(key);
+                }
+                analytics.load = function (key, e) {
+                  var t = document.createElement("script");
+                  t.type = "text/javascript";
+                  t.async = !0;
+                  t.src =
+                    "https://cdn.segment.com/analytics.js/v1/" +
+                    key +
+                    "/analytics.min.js";
+                  var n = document.getElementsByTagName("script")[0];
+                  n.parentNode.insertBefore(t, n);
+                  analytics._loadOptions = e;
+                };
+                analytics._writeKey = "IqDs0syyZFejyyjZ9ix6G3AZHwpUkKYl";
+                analytics.SNIPPET_VERSION = "4.15.3";
+                analytics.load("IqDs0syyZFejyyjZ9ix6G3AZHwpUkKYl");
+                analytics.page();
+              }
+          })()
+        }
+      </script>
       <script src="https://www.googleoptimize.com/optimize.js?id=OPT-TGB62G7"></script>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=L6uzByobzaeXDxTEp4EKyDXuK0LW8yTa`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${ANALYTICS}`}
       />
       <Script strategy="lazyOnload">
         {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'L6uzByobzaeXDxTEp4EKyDXuK0LW8yTa');
+        gtag('config', '${ANALYTICS}');
       `}
       </Script>
       <Component {...pageProps} />
