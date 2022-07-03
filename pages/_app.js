45a1dc2940f2227c2922b8067fb19a568f834d76
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-07-03 15:11:00 +0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -28,73 +28,19 @@ function MyApp({ Component, pageProps }) {
           type: "website",
         }}
       />
-      {/* L6uzByobzaeXDxTEp4EKyDXuK0LW8yTa */}
-      <Script>
-        {`
-          !(function () {
-            var analytics = (window.analytics = window.analytics || []);
-            if (!analytics.initialize)
-              if (analytics.invoked)
-                window.console &&
-                  console.error &&
-                  console.error("Segment snippet included twice.");
-              else {
-                analytics.invoked = !0;
-                analytics.methods = [
-                  "trackSubmit",
-                  "trackClick",
-                  "trackLink",
-                  "trackForm",
-                  "pageview",
-                  "identify",
-                  "reset",
-                  "group",
-                  "track",
-                  "ready",
-                  "alias",
-                  "debug",
-                  "page",
-                  "once",
-                  "off",
-                  "on",
-                  "addSourceMiddleware",
-                  "addIntegrationMiddleware",
-                  "setAnonymousId",
-                  "addDestinationMiddleware",
-                ];
-                analytics.factory = function (e) {
-                  return function () {
-                    var t = Array.prototype.slice.call(arguments);
-                    t.unshift(e);
-                    analytics.push(t);
-                    return analytics;
-                  };
-                };
-                for (var e = 0; e < analytics.methods.length; e++) {
-                  var key = analytics.methods[e];
-                  analytics[key] = analytics.factory(key);
-                }
-                analytics.load = function (key, e) {
-                  var t = document.createElement("script");
-                  t.type = "text/javascript";
-                  t.async = !0;
-                  t.src =
-                    "https://cdn.segment.com/analytics.js/v1/" +
-                    key +
-                    "/analytics.min.js";
-                  var n = document.getElementsByTagName("script")[0];
-                  n.parentNode.insertBefore(t, n);
-                  analytics._loadOptions = e;
-                };
-                analytics._writeKey = "IqDs0syyZFejyyjZ9ix6G3AZHwpUkKYl";
-                analytics.SNIPPET_VERSION = "4.15.3";
-                analytics.load("IqDs0syyZFejyyjZ9ix6G3AZHwpUkKYl");
-                analytics.page();
-              }
-          })()
-          `}
-      </Script>
-      <script src="https://www.googleoptimize.com/optimize.js?id=OPT-TGB62G7"></script>
+      <script
+        src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+        async=""
+      ></script>
+      <script>
+        {`window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "77731724-8ce0-4b47-aad9-8add0056e47e",
+    });
+  });`}
+      </script>
+      {/* <script src="https://www.googleoptimize.com/optimize.js?id=OPT-TGB62G7"></script> */}
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${ANALYTICS}`}
