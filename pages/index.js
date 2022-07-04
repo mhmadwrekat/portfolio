17645785e0c97e6336b8f9a71024c3b2db606d06
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-07-04 13:11:27 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,49 @@ const index = (props) => {
   /************************************************************************************** */
   /************************************************************************************** */
   /************************************************************************************** */
+  var sendNotification = function (data) {
+    var headers = {
+      "Content-Type": "application/json; charset=utf-8",
+      Authorization: "Basic MGIwYzdhZGMtZmU2ZC00M2ZkLTliNjEtMDRkNTE2ZmE3OGE0",
+    };
 
+    var options = {
+      host: "onesignal.com",
+      port: 443,
+      path: "/api/v1/notifications",
+      method: "POST",
+      headers: headers,
+    };
+
+    var https = require("https");
+    var req = https.request(options, function (res) {
+      res.on("data", function (data) {
+        console.log("Response:");
+        console.log(JSON.parse(data));
+      });
+    });
+
+    req.on("error", function (e) {
+      console.log("ERROR:");
+      console.log(e);
+    });
+
+    req.write(JSON.stringify(data));
+    req.end();
+  };
+
+  var message = {
+    app_id: "77731724-8ce0-4b47-aad9-8add0056e47e",
+    contents: { en: "English Message" },
+    channel_for_external_user_ids: "push",
+    include_external_user_ids: [
+      "6392d91a-b206-4b7b-a620-cd68e32c3a76",
+      "76ece62b-bcfe-468c-8a78-839aeaa8c5fa",
+      "8e0f21fa-9a5a-4ae7-a9a6-ca1f24294b86",
+    ],
+  };
+
+  sendNotification(message);
   /************************************************************************************** */
   /************************************************************************************** */
   /************************************************************************************** */
@@ -41,7 +83,7 @@ const index = (props) => {
   return (
     <React.Fragment>
       {/* {console.log("SSR --> ", props.weather)} */}
-      <Track />
+      {/* <Track /> */}
       <section translate="no">
         <Head_comp />
         <Profile weather={props.weather} />
