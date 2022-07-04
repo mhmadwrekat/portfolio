0bbfe33eb9b3e9a48068d94cb4c68bdd5be63b81
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-07-04 12:41:16 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -29,50 +29,50 @@ const index = (props) => {
   /************************************************************************************** */
   /************************************************************************************** */
   /************************************************************************************** */
-  const sendNotification = (data) => {
-    var headers = {
-      "Content-Type": "application/json; charset=utf-8",
-      Authorization: "Basic MGIwYzdhZGMtZmU2ZC00M2ZkLTliNjEtMDRkNTE2ZmE3OGE0",
-    };
-    var options = {
-      host: "onesignal.com",
-      port: 443,
-      path: "/api/v1/notifications",
-      method: "POST",
-      headers: headers,
-    };
+  // const sendNotification = (data) => {
+  //   var headers = {
+  //     "Content-Type": "application/json; charset=utf-8",
+  //     Authorization: "Basic MGIwYzdhZGMtZmU2ZC00M2ZkLTliNjEtMDRkNTE2ZmE3OGE0",
+  //   };
+  //   var options = {
+  //     host: "onesignal.com",
+  //     port: 443,
+  //     path: "/api/v1/notifications",
+  //     method: "POST",
+  //     headers: headers,
+  //   };
 
-    var https = require("https");
-    var req = https.request(options, function (res) {
-      res.on("data", function (data) {
-        console.log("Response:");
-        console.log(JSON.parse(data));
-      });
-    });
+  //   var https = require("https");
+  //   var req = https.request(options, function (res) {
+  //     res.on("data", function (data) {
+  //       console.log("Response:");
+  //       console.log(JSON.parse(data));
+  //     });
+  //   });
 
-    req.on("error", function (e) {
-      console.log("ERROR:");
-      console.log(e);
-    });
+  //   req.on("error", function (e) {
+  //     console.log("ERROR:");
+  //     console.log(e);
+  //   });
 
-    req.write(JSON.stringify(data));
-    // setID(data);
-    req.end();
-  };
+  //   req.write(JSON.stringify(data));
+  //   // setID(data);
+  //   req.end();
+  // };
 
-  var message = {
-    app_id: "77731724-8ce0-4b47-aad9-8add0056e47e",
-    contents: {
-      en: "😊 اهلا وسهلا بك في موقعي الشخصي, لا تتردد في اعطاء رأيك 😊",
-    },
-    include_player_ids: [
-      "2225210d-fce2-4074-b9bd-f9943516ae71",
-      "8fbfc53a-b3e7-4933-aa90-8ed77a2bb84b",
-    ],
-    // included_segments: ["Subscribed Users"],
-  };
+  // var message = {
+  //   app_id: "77731724-8ce0-4b47-aad9-8add0056e47e",
+  //   contents: {
+  //     en: "😊 اهلا وسهلا بك في موقعي الشخصي, لا تتردد في اعطاء رأيك 😊",
+  //   },
+  //   include_player_ids: [
+  //     "2225210d-fce2-4074-b9bd-f9943516ae71",
+  //     "8fbfc53a-b3e7-4933-aa90-8ed77a2bb84b",
+  //   ],
+  //   // included_segments: ["Subscribed Users"],
+  // };
 
-  sendNotification(message);
+  // sendNotification(message);
   /************************************************************************************** */
   /************************************************************************************** */
   /************************************************************************************** */
