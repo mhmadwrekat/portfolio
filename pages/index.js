a373dd6396bfae2196cb3ce6d690d3e439cf2949
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-08-14 15:58:23 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -33,6 +33,7 @@ const index = (props) => {
   // props.countryCode && console.log(props.countryCode);
   // props.f && console.log(props.f);
   console.log(process.env.NEXT_PUBLIC_AA);
+
   // const [id, setID] = useState();
   // let deviceId = props.deviceId && props.deviceId;
   // /************************************************************************************** */
