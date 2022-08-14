34efe3420fb2c430c774c0f0f02d8e11e799539f
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-08-14 15:52:06 +0300

    update

--- next.config.js
+++ next.config.js
@@ -4,51 +4,51 @@ module.exports = {
     defaultLocale: "en",
   },
   productionBrowserSourceMaps: true,
-  entry: "./src/js/main.js",
-  output: {
-    path: __dirname + "/dist/",
-    filename: "bundle.js",
-    publicPath: "/",
-  },
-  devServer: {
-    inline: false,
-    contentBase: "./dist",
-  },
-  rewrites: async () => [
-    {
-      source: "/public/myfile.html",
-      destination: "/pages/api/myfile.js",
-    },
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        query: {
-          presets: ["es2015", "react"],
-        },
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/:all*(svg|jpg|png|webp)",
-        locale: false,
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=9999999999, must-revalidate",
-          },
-        ],
-      },
-    ];
-  },
+  // entry: "./src/js/main.js",
+  // output: {
+  //   path: __dirname + "/dist/",
+  //   filename: "bundle.js",
+  //   publicPath: "/",
+  // },
+  // devServer: {
+  //   inline: false,
+  //   contentBase: "./dist",
+  // },
+  // rewrites: async () => [
+  //   {
+  //     source: "/public/myfile.html",
+  //     destination: "/pages/api/myfile.js",
+  //   },
+  // ],
+  // module: {
+  //   loaders: [
+  //     {
+  //       test: /\.jsx?$/,
+  //       exclude: /(node_modules|bower_components)/,
+  //       loader: "babel-loader",
+  //       query: {
+  //         presets: ["es2015", "react"],
+  //       },
+  //     },
+  //   ],
+  // },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/:all*(svg|jpg|png|webp)",
+  //       locale: false,
+  //       headers: [
+  //         {
+  //           key: "Cache-Control",
+  //           value: "public, max-age=9999999999, must-revalidate",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 
   images: {
-    minimumCacheTTL: 9999999999,
+    // minimumCacheTTL: 9999999999,
     formats: ["image/avif", "image/webp"],
   },
 };
