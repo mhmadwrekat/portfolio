d950e5cb34d20f1118a84c61bf66b08116358330
mhmadwrekat <mhmmadwreekkat@gmail.com>
2022-08-14 16:40:50 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -32,8 +32,9 @@ export async function getServerSideProps(context) {
 const index = (props) => {
   // props.countryCode && console.log(props.countryCode);
   // props.f && console.log(props.f);
+  //
   console.log(process.env.NEXT_PUBLIC_AA);
-
+  //
   // const [id, setID] = useState();
   // let deviceId = props.deviceId && props.deviceId;
   // /************************************************************************************** */
